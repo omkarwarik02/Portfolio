6a75450cf2efa5a90a37c28f6c6eaa1b386fbe05
OMKAR AVINASH WARIK <omkarwarik1204@gmail.com>
2026-06-11 01:52:41 +0530

    Fixed navbar for mobile view

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
       <div
       onMouseEnter={()=> setExpanded(true)}
       onMouseLeave={()=>setExpanded(false)} 
-      className={`bg-[#111111] text-white flex justify-around  rounded-full items-center  gap-4 shadow-lg transition-all duration-500 ease-in-out px-5 py-2.5 ${expanded ? 'w-[420px]' : 'w-[290px]'}`}>
+      className={`bg-[#111111] text-white flex justify-around rounded-full items-center gap-4 shadow-lg transition-all duration-500 ease-in-out px-5 py-2.5 w-auto ${expanded ? 'md:w-[420px]' : 'md:w-[290px]'}`}>
         
           <span className="font-semibold text-lg">OW</span>
           <a href="#about" className=" hidden md:block text-xs text-white/80 hover:text-white">
@@ -25,7 +25,7 @@ function Navbar() {
           <a
             href="https://github.com/omkarwarik02"
             target="_blank"
-            className="text-xs text-white/80 hover:text-white"
+            className="hidden md:inline text-xs text-white/80 hover:text-white"
           >
             &lt;/&gt;
           </a>
